7484eb4271fd396099d05e596d42bb2c3a0a508c
Brian Payne <bwpayne@gmail.com>
2018-05-05 19:42:06 -0700

    Added logs to be sent to logdna

--- app/config/parameters_prod.php
+++ app/config/parameters_prod.php
@@ -3,3 +3,4 @@ $secrets = json_decode(file_get_contents($_SERVER['APP_SECRETS']), true);
 
 $container->setParameter('telegram_token', $secrets['CUSTOM']['TELEGRAM_TOKEN']);
 $container->setParameter('redis_password', $secrets['CUSTOM']['REDIS_PASSWORD']);
+$container->setParameter('logdna_token', $secrets['CUSTOM']['LOGDNA_TOKEN']);
